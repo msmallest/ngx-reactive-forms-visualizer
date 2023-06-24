6895c612c28f54ae15c6fb6c04c7380781bfbc6b
Michael Small <chickenhat22@gmail.com>
2023-06-24 10:36:46 -0500

    Add example of basic `<pre>` to test app

--- projects/testing/src/app/app.component.ts
+++ projects/testing/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'testing';
+export class AppComponent implements OnInit {
+  exampleForm = this.fb.group({
+    name: [''],
+    town: [''],
+    addressNumber: [null],
+  });
+  snippet = '<pre>{{form.value | json}}</pre>';
+  constructor(private fb: NonNullableFormBuilder) {}
+
+  ngOnInit() {}
 }
