f8a83ac3489a78188294d7330607b3571524f85b
Michael Small <chickenhat22@gmail.com>
2023-06-24 10:42:43 -0500

    Import/Embed boilerplate library component in test app

--- projects/testing/src/app/app.module.ts
+++ projects/testing/src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxReactiveFormsVisualizerModule } from 'ngx-reactive-forms-visualizer';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxReactiveFormsVisualizerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
