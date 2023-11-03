07a05012e69fc460a9a7eba9acd5642ec1e5ee2b
Michael Small <michael.small@relationshipone.com>
2023-11-03 15:22:35 -0500

    Convert testing app to standalone

--- projects/testing/src/app/app.component.ts
+++ projects/testing/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NonNullableFormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BasicPreComponent, ValidationStatusTreeComponent } from 'ngx-reactive-forms-visualizer';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgIf, JsonPipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        NgIf,
+        MatDividerModule,
+        MatCardModule,
+        BasicPreComponent,
+        ValidationStatusTreeComponent,
+        JsonPipe,
+    ],
 })
 export class AppComponent implements OnInit {
     exampleForm = this.fb.group({
--- projects/testing/src/main.ts
+++ projects/testing/src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BasicPreComponent, ValidationStatusTreeComponent } from 'ngx-reactive-forms-visualizer';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDividerModule, MatCardModule),
+        provideAnimations()
+    ]
+})
     .catch((err) => console.error(err));
