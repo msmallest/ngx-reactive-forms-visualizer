07cfe81b72a8bad14fae3a020fc471b9075955bf
Michael Small <michael.small@relationshipone.com>
2023-11-04 20:21:53 -0500

    Add betterer

---
+++ .betterer.js
@@ -0,0 +1,52 @@
+const { BettererFileTest } = require('@betterer/betterer');
+const { BettererError } = require('@betterer/errors');
+const fs = require('node:fs').promises;
+
+function regexp(pattern, issueMessage = 'RegExp match') {
+    if (!pattern) {
+        throw new BettererError('for `@betterer/regexp` to work, you need to provide a RegExp, e.g. `/^foo$/`. ❌');
+    }
+
+    return new BettererFileTest(async (filePaths, fileTestResult) => {
+        if (!filePaths.length) {
+            return;
+        }
+
+        pattern = new RegExp(pattern.source, pattern.flags.includes('g') ? pattern.flags : `${pattern.flags}g`);
+        await Promise.all(
+            filePaths.map(async (filePath) => {
+                const fileText = await fs.readFile(filePath, 'utf8');
+                const matches = getFileMatches(pattern, fileText);
+                if (matches.length === 0) {
+                    return;
+                }
+                const file = fileTestResult.addFile(filePath, fileText);
+                matches.forEach((match) => {
+                    const [matchText] = match;
+                    const start = match.index;
+                    file.addIssue(start, start + matchText.length, issueMessage);
+                });
+            })
+        );
+    });
+}
+
+function getFileMatches(pattern, fileText) {
+    const matches = [];
+
+    let currentMatch;
+    do {
+        currentMatch = pattern.exec(fileText);
+        if (currentMatch) {
+            matches.push(currentMatch);
+        }
+    } while (currentMatch);
+
+    return matches;
+}
+
+// TODO - run:
+// npm run betterer
+module.exports = {
+    'No TODOs': () => regexp(/(\/\/\s*TODO)/i).include('./**/*.ts')
+};
