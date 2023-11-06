0fc053c36b62a2489eb4325d0f1b2f0d580791c0
Michael Small <michael.small@relationshipone.com>
2023-11-06 10:22:26 -0600

    Create simple configurable storybook example in lib

---
+++ projects/ngx-reactive-forms-visualizer/src/lib/configurable-component/configurable-component.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfigurableComponentComponent } from './configurable-component.component';
+
+describe('ConfigurableComponentComponent', () => {
+  let component: ConfigurableComponentComponent;
+  let fixture: ComponentFixture<ConfigurableComponentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ConfigurableComponentComponent]
+    });
+    fixture = TestBed.createComponent(ConfigurableComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- projects/ngx-reactive-forms-visualizer/src/stories/basic-pre.stories.ts
+++ projects/ngx-reactive-forms-visualizer/src/stories/basic-pre.stories.ts
@@ -1,38 +1,19 @@
-// Import the actual component, with absolute path
-
-// Basic storybook dependencies
 import type { Meta, Story } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
-import { userEvent, within } from '@storybook/testing-library';
-
-// Most common dependencies
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-// Sometimes common dependencies
-import { RouterTestingModule } from '@angular/router/testing';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { BasicPreComponent } from 'ngx-reactive-forms-visualizer';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BasicPreComponent } from '../lib/basic-pre/basic-pre.component';
 
 export default {
-    // Prefix App's name Or 'Library' for hierarchy
-    // Example:
-    //    title: 'Library/Divider'
-    //    titel: 'Appcloud Admin Portal/Login'
     title: 'Basic Pre',
     component: BasicPreComponent,
     decorators: [
         moduleMetadata({
-            // These are the most common imports we use; remove unneeded ones as it makes sense, or add your own
             imports: [BasicPreComponent],
         }),
     ],
 } as Meta<BasicPreComponent>;
 
-const GroupingLogicComplexTemplate: Story<BasicPreComponent> = (args: BasicPreComponent) => {
+const BasicPre: Story<BasicPreComponent> = (args: BasicPreComponent) => {
     const formGroup = new FormGroup({
         type: new FormControl('simple'),
         groupingLogic: new FormControl('and'),
@@ -48,10 +29,5 @@ const GroupingLogicComplexTemplate: Story<BasicPreComponent> = (args: BasicPreCo
     };
 };
 
-export const SimpleState = GroupingLogicComplexTemplate.bind({});
+export const SimpleState = BasicPre.bind({});
 SimpleState.args = {};
-// SimpleState.play = async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
-//     await expect(canvas.getByText('AND')).toBeInTheDocument();
-//     await expect(canvas.getByText('OR')).toBeInTheDocument();
-// };
