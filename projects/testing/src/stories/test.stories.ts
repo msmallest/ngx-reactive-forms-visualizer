0e4fc730d64adc0d7cf5fcd7cef6dde5fcb0c02e
Michael Small <michael.small@relationshipone.com>
2023-11-06 10:43:01 -0600

    Add storybook to `testing`

---
+++ projects/testing/src/stories/test.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, Story } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { FormControl, FormGroup } from '@angular/forms';
+import { TestComponent } from '../app/test/test.component';
+
+export default {
+    title: 'Test',
+    component: TestComponent,
+    decorators: [
+        moduleMetadata({
+            imports: [TestComponent],
+        }),
+    ],
+} as Meta<TestComponent>;
+
+const BasicPre: Story<TestComponent> = (args: TestComponent) => {
+    return {
+        component: TestComponent,
+        template: `<app-test />`,
+        props: {
+            ...args,
+        },
+    };
+};
+
+export const SimpleState = BasicPre.bind({});
+SimpleState.args = {};
